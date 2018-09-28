9409049ca589ec9f1af4609785bcc999c726e67c
Your Namegit <strangn10@gmail.com>
2018-09-28 11:44:35 -0400

    first push into prsweb

---
+++ PRSWeb/src/main/java/com/prsweb/PrsWebApplication.java
@@ -0,0 +1,12 @@
+package com.prsweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrsWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrsWebApplication.class, args);
+	}
+}
