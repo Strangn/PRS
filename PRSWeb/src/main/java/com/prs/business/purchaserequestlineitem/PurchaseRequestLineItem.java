199bff7bf9007b366eaa9daf7656ce980497c20b
Your Namegit <strangn10@gmail.com>
2018-10-29 14:57:07 -0400

    updateprliannotations

--- PRSWeb/src/main/java/com/prs/business/purchaserequestlineitem/PurchaseRequestLineItem.java
+++ PRSWeb/src/main/java/com/prs/business/purchaserequestlineitem/PurchaseRequestLineItem.java
@@ -1,8 +1,22 @@
 package com.prs.business.purchaserequestlineitem;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity 
 public class PurchaseRequestLineItem {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	@ManyToOne
+	@JoinColumn(name="purchaseRequestID")
 	private int purchaseRequestId;
+	@ManyToOne
+	@JoinColumn(name="productID")
 	private int productId;
 	private int quantity;
 	
